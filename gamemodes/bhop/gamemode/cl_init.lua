cd8075883d7814280d7f5be2e777e5d01f187f5b
FiBzY <58349075+FiBzYDev@users.noreply.github.com>
2024-02-06 21:20:32 -0500

    Add files via upload

--- gamemodes/bhop/gamemode/cl_init.lua
+++ gamemodes/bhop/gamemode/cl_init.lua
@@ -1222,6 +1222,15 @@ hook.Add( "OnPlayerChat", "TaggedChat", ChatTag )
 
 local function EntityCheckPost( ply )
 	hook.Remove( "PostDrawOpaqueRenderables", "PlayerMarkers" )
+	hook.Remove( "PostDrawOpaqueRenderables", "PlayerMarkers" )
+	hook.Remove( "PlayerTick", "PlayerTick" )
+	hook.Remove( "SetupMove", "SetupMove" )
+	hook.Remove( "StartMove", "StartMove" )
+	hook.Remove( "FinishMove", "FinishMove" )
+	hook.Remove( "CreateMove", "CreateMove" )
+	hook.Remove( "Tick", "Tick" )
+	hook.Remove( "Think", "Think" )
+
 	RunConsoleCommand( "sl_targetids", 0 )
 end
 hook.Add( "InitPostEntity", "StartEntityCheck", EntityCheckPost )
